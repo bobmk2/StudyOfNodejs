a09fe82f9bd2d2ddadcc40878df9c7df039e2346
BobMk2 <too.good.to.see.0806@gmail.com>
2013-12-07 20:06:11 +0000

    占い結果のページを表示

--- FirstNodejs/DesertFox/requestHandlers.js
+++ FirstNodejs/DesertFox/requestHandlers.js
@@ -6,6 +6,8 @@
  */
 
 var fs = require('fs');
+var querystring = require('querystring');
+var crypto = require('crypto');
 
 function uranai(response, request) {
 	console.log("Uranai start");
@@ -13,34 +15,6 @@ function uranai(response, request) {
 	if (request.method == 'GET') {
 		// GET
 		response.writeHead(200, {"Content-Type": "text/html"});
-
-		var htmlHeader = '<!DOCTYPE html>\
-		 <html lang="ja">\
-		 <head>\
-		   <meta charset="utf-8">\
-		   <title>ノード占い</title>\
-		   <style>\
-		     .content {\
-		       width: 480px;\
-		       text-align: center;\
-		       border: 4px solid lightblue;\
-		       padding: 4px;\
-		       margin: 16px auto;\
-		     }\
-		     .main-form div {margin-bottom: 4px;}\
-		     .result {\
-		       display: block;\
-		       font-size: 200%;\
-		       color: red;\
-		       margin: 4px auto;\
-		       border: 1px solid;\
-		       width: 4em;\
-		     }\
-		   </style>\
-		 </head>\
-		 <body>\
-		 <div class="content">\
-		 <h1>ノード占い</h1>';
 
 		var htmlMainForm = '<div class="main-form">\
 			<form method="post" action="/uranai">\
@@ -62,20 +36,67 @@ function uranai(response, request) {
 			</form>\
 		</div>';
 
-		var htmlFooter = '</div></body></html>';
-
 		response.write(htmlHeader);
 		response.write(htmlMainForm);
 		response.write(htmlFooter);
 		response.end();
+	} else if (request.method == 'POST') {
+		// POST
+		response.writeHead(200, {"Content-Type": "text/html"});
+
+		request.data = '';
+		request.on('data', function (chunk) {
+			request.data += chunk;
+		});
+		request.on('end', function () {
+			var query = querystring.parse(request.data);
+
+			var seed = query.name + query.year + query.month + query.day + query.sex;
+			hash = crypto.createHash('md5');
+			hash.update(seed);
+			var hashValue = hash.digest('hex');
+
+			var fortuneKey = Number('0x' + hashValue.slice(0, 2));
+			var result = '';
+			if (fortuneKey < 10) {
+				result = '大凶';
+			} else if (fortuneKey < 50) {
+				result = '凶';
+			} else if (fortuneKey < 100) {
+				result = '末吉';
+			} else if (fortuneKey < 150) {
+				result = '吉';
+			} else if (fortuneKey < 245) {
+				result = '中吉';
+			} else {
+				result = '大吉';
+			}
+
+			var resultStr = '<div><p>'
+				+ escapeHtmlSpecialChar(query.year) + '年'
+				+ escapeHtmlSpecialChar(query.month) + '月'
+				+ escapeHtmlSpecialChar(query.day) + '日生まれの'
+				+ escapeHtmlSpecialChar(query.name) + 'さん（'
+				+ ((query.sex == 'male') ? '男性' : '女性')
+				+ '）の運勢は……'
+				+ '<span class="result">'
+				+ result + '</span>'
+				+ 'です。</p></div>'
+				+ '<a href="/">トップに戻る</a>';
+
+			response.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+			response.write(htmlHeader);
+			response.write(resultStr);
+			response.write(htmlFooter);
+			response.end();
+		});
 	}
-
-
 }
 
 function image404(response) {
 	fs.readFile('./tmp/desert_fox.png', 'binary', function (error, file) {
 		if (error) {
+			console.log('Error : ' + error);
 			response.writeHead(500, {"Content-Type": "text/plain"});
 			response.write(error + "\n");
 			response.end();
@@ -99,6 +120,35 @@ function escapeHtmlSpecialChar(html) {
 	}
 }
 
+var htmlHeader = '<!DOCTYPE html>\
+		<html lang="ja">\
+			<head>\
+			<meta charset="utf-8">\
+			<title>ノード占い</title>\
+			<style>\
+				.content {\
+				width: 480px;\
+				text-align: center;\
+				border: 4px solid lightblue;\
+				padding: 4px;\
+				margin: 16px auto;\
+			}\
+			.main-form div {margin-bottom: 4px;}\
+			.result {\
+				display: block;\
+				font-size: 200%;\
+				color: red;\
+				margin: 4px auto;\
+				border: 1px solid;\
+				width: 4em;\
+			}\
+			</style>\
+		</head>\
+		<body>\
+		<div class="content">\
+		<h1>ノード占い</h1>';
+
+var htmlFooter = '</div></body></html>';
 
 exports.uranai = uranai;
 exports.image404 = image404;
